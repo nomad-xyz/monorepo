961a3579b1c54f7ad6185afb638ad8768f50621f
ErinHales <mcbridee093@gmail.com>
2022-03-07 12:38:22 -0700

    add: initial test

--- tests/multi-provider/index.test.ts
+++ tests/multi-provider/index.test.ts
@@ -1,15 +1,26 @@
 import { expect } from 'chai';
+import { MultiProvider } from '@nomad-xyz/multi-provider';
+import { ethers } from 'ethers';
 
+interface Domain {
+  name: string;
+  domain: number;
+}
 describe('multi-provider', async () => {
-  // before(async () => {
-  //
-  // });
+  it('registers domains properly', async () => {
+    const provider = await ethers.getDefaultProvider()
 
-  // beforeEach(async () => {
-  //
-  // });
-
-  it('', async () => {
+    const chainADomain: Domain = {
+      name: 'a',
+      domain: 1000,
+    }
+    const chainBDomain: Domain = {
+      name: 'b',
+      domain: 2000,
+    }
+    const mp = new MultiProvider<Domain>();
+    mp.registerDomain(chainADomain);
+    mp.registerDomain(chainBDomain);
     expect(true).to.be.true;
   });
 });
