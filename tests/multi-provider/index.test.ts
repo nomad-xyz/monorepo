1dbc860fc817946a326f1b05ac2771bf5f50c849
ErinHales <mcbridee093@gmail.com>
2022-03-07 11:03:32 -0700

    add: packages, test script

---
+++ tests/multi-provider/index.test.ts
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+
+describe('multi-provider', async () => {
+  // before(async () => {
+  //
+  // });
+
+  // beforeEach(async () => {
+  //
+  // });
+
+  it('', async () => {
+    expect(true).to.be.true;
+  });
+});
