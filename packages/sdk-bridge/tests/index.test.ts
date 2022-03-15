4617e39f5ec2fb0d7d467e74443d703df12791dc
Daniil Naumetc <daniil.naumetc@gmail.com>
2022-03-15 19:45:37 +0100

    test: bridge addition

--- packages/sdk-bridge/tests/index.test.ts
+++ packages/sdk-bridge/tests/index.test.ts
@@ -1,133 +1,35 @@
 import { describe, it } from 'mocha';
 
+import { BridgeContext } from '@nomad-xyz/sdk-bridge';
+import * as config from '@nomad-xyz/configuration';
+
 import { expect } from 'chai';
+import { NomadContext } from '@nomad-xyz/sdk';
+
+const ENVIRONMENTS = ['staging', 'production'];
 
 describe('sdk-bridge', async () => {
   describe('BridgeContext', () => {
-    // before(async () => {
-    //
-    // });
+    it('Is properly instantiated from a NomadConfig and then NomadContext', async () => {
+      for (const env of ENVIRONMENTS) {
+        const conf = config.getBuiltin(env);
+        const nomadContext = new NomadContext(conf);
+        const context = BridgeContext.fromNomadContext(nomadContext);
+        const domains = conf.networks;
 
-    // beforeEach(async () => {
-    //
-    // });
+        // Register providers and test bridgeRouter, tokenRegistry and ethHelper
+        for (const domain of domains) {
+          const rpc = conf.rpcs[domain][0];
+          context.registerRpcProvider(domain, rpc);
 
-    describe('fromNomadContext()', () => {
-      it('returns a bridge given a context', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-    });
+          const bridge = context.getBridge(domain);
+          const confBridge = conf.bridge[domain];
 
-    describe('getBridge()', () => {
-      it('gets a bridge', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-    });
-
-    describe('mustGetBridge()', () => {
-      it('gets a bridge', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-
-      it('throws if it cannot get a bridge', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-    });
-
-    describe('resolveRepresentation()', () => {
-      it('returns a single token representation', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-
-      it('throws if there is no provider or signer for a domain', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-    });
-
-    describe('resolveRepresentations()', () => {
-      it('returns all token representations', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-    });
-
-    describe('resolveCanonicalIdentifier()', () => {
-      it('returns details of the canonical token', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-
-      it('throws if the token if unknown to the bridge router on its domain', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-    });
-
-    describe('resolveCanonicalToken()', () => {
-      it('returns an interface for the token', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-
-      it("throws if the token doesn't exist", () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-
-      it('throws if the cannot resolve on its own domain', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-    });
-
-    describe('send()', () => {
-      it('returns a message representing the transfer', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-
-      it('throws if the token is not available on the origin', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-
-      it('throws if there is no signer on the origin', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-
-      it('throws if there is no message to return', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-    });
-
-    describe('sendNative()', () => {
-      it('returns a message representing the transfer', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-
-      it('throws if there was an attempt to send token to failed home', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-
-      it('throws if there is no ethHelper on the origin', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
-
-      it('throws if there is no message to return', () => {
-        // TODO
-        expect(true).to.be.true;
-      });
+          expect(bridge.bridgeRouter.address).to.equal(confBridge.bridgeRouter.proxy);
+          expect(bridge.tokenRegistry.address).to.equal(confBridge.tokenRegistry.proxy);
+          expect(bridge.ethHelper.address).to.equal(confBridge.ethHelper);
+        }
+      }
     });
   });
 });
