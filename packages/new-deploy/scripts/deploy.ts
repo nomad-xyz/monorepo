81626abd56c9ab33c40631cdcb39256cf9c9aea5
Anna Carroll <anna.s.carroll@gmail.com>
2022-03-28 13:59:21 +0200

    get deployer key from .env

--- packages/new-deploy/scripts/deploy.ts
+++ packages/new-deploy/scripts/deploy.ts
@@ -3,6 +3,13 @@ import * as config from '@nomad-xyz/configuration';
 import * as ethers from 'ethers';
 import { NonceManager } from '@ethersproject/experimental';
 import fs from "fs";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
+if (!DEPLOYER_PRIVATE_KEY) {
+    throw new Error('Add DEPLOYER_PRIVATE_KEY to .env');
+}
 
 run();
 
@@ -13,7 +20,6 @@ run();
 * re-publish config
 * */
 const config = {}; // TODO: get published (or local??) config
-const DEPLOYER_PRIVATE_KEY = ""; // TODO: get from .env - also, do we need different private keys for each chain??
 const OVERRIDES = {}; // TODO write local overrides config
 
 async function run() {
