c661a79211894f7188301cfcb9a0e51af37f4a07
Anna Carroll <anna.s.carroll@gmail.com>
2022-04-05 00:26:58 +0200

    put order back

--- packages/new-deploy/src/DeployContext.ts
+++ packages/new-deploy/src/DeployContext.ts
@@ -113,6 +113,138 @@ export default class DeployContext extends MultiProvider<config.Domain> {
     this._data = config.addBridge(this.data, name, bridge);
   }
 
+  pushVerification(
+      nameOrDomain: string | number,
+      verification: Verification,
+  ): void {
+    const name = this.resolveDomainName(nameOrDomain);
+    if (!this.verification.has(name)) this.verification.set(name, []);
+
+    const net = this._verification.get(name);
+    if (net) net.push(verification);
+  }
+
+  mustGetVerification(nameOrDomain: string | number): readonly Verification[] {
+    const domain = this.resolveDomainName(nameOrDomain);
+    const verification = this.verification.get(domain);
+    if (!verification)
+      throw new Error(
+          `Verification with name ${nameOrDomain} for domain ${domain} is not defined`,
+      );
+
+    return verification;
+  }
+
+  protected async deployCore(domain: config.Domain): Promise<void> {
+    this.addDomain(domain);
+
+    const core = new CoreContracts(this, domain.name);
+    await core.recordStartBlock();
+
+    await Promise.all([
+      core.deployUpgradeBeaconController(),
+      core.deployUpdaterManager(),
+      core.deployXAppConnectionManager(),
+    ]);
+
+    await core.deployHome();
+    await core.deployGovernanceRouter();
+
+    // all contracts deployed
+    const complete = core.complete();
+    this.addCore(domain.name, complete);
+  }
+
+  protected async deployBridge(name: string): Promise<void> {
+    this.mustGetCore(name).complete(); // assert that the core is totally deployed
+
+    const bridge = new BridgeContracts(this, name);
+    await bridge.recordStartBlock();
+
+    await bridge.deployTokenUpgradeBeacon();
+    await bridge.deployTokenRegistry();
+    await bridge.deployBridgeRouter();
+    await bridge.deployEthHelper();
+
+    this.addBridge(name, bridge.complete());
+  }
+
+  /// Deploys all configured Cores
+  async ensureCores(): Promise<void> {
+    const networksToDeploy = this.networks.filter((net) => !this.cores[net]);
+    await Promise.all(
+        networksToDeploy.map((net) =>
+            this.deployCore(this.mustGetDomainConfig(net)),
+        ),
+    );
+  }
+
+  // Deploys all configured connections and enrolls them if possible.
+  // For any connection that cannot be enrolled, outputs the governance
+  // action required to enroll them
+  async ensureCoreConnections(): Promise<ethers.PopulatedTransaction[]> {
+    // ensure all core contracts are deployed
+    await this.ensureCores();
+    // ensure all core contracts are enrolled in each other
+    const enrollTransactions = await Promise.all(
+        this.networks.map(async (network) => {
+          const core = this.mustGetCore(network);
+          const name = this.resolveDomainName(network);
+          const remoteDomains = this.data.protocol.networks[name]?.connections;
+          // the following "unreachable" error performs type-narrowing for the compiler
+          if (!remoteDomains) throw new Error('unreachable');
+          const txns = await Promise.all(
+              remoteDomains.map((remote) => core.enrollRemote(remote)),
+          );
+          return txns.flat();
+        }),
+    );
+    return enrollTransactions.flat();
+  }
+
+  /// Deploys all configured bridges.
+  async ensureBridges(): Promise<void> {
+    const toDeploy = this.networks.filter((net) => !this.bridges[net]);
+    await Promise.all(toDeploy.map((net) => this.deployBridge(net)));
+  }
+
+  async ensureBridgeConnections(): Promise<ethers.PopulatedTransaction[]> {
+    // first, ensure all bridge contracts are deployed
+    await this.ensureBridges();
+    // next, ensure all bridge contracts are enrolled in each other
+    // and all custom tokens are also enrolled
+    const enrollTransactions = await Promise.all(
+        this.networks.map(
+            async (network): Promise<ethers.PopulatedTransaction[]> => {
+              const bridge = this.mustGetBridge(network);
+              const name = this.resolveDomainName(network);
+              const remoteDomains = this.data.protocol.networks[name]?.connections;
+              // the following "unreachable" error performs type-narrowing for the compiler
+              if (!remoteDomains) throw new Error('unreachable');
+              const allEnrollRouterTxns = await Promise.all(
+                  remoteDomains.map((remote) => bridge.enrollBridgeRouter(remote)),
+              );
+              const enrollTxns = allEnrollRouterTxns.flat();
+              // deploy and enroll custom tokens
+              const txns = await bridge.deployCustomTokens();
+              enrollTxns.push.apply(txns);
+              return enrollTxns;
+            },
+        ),
+    );
+    return enrollTransactions.flat();
+  }
+
+  async relinquishOwnership(): Promise<void> {
+    // relinquish deployer control
+    await Promise.all([
+      ...this.networks.map((network) => this.mustGetCore(network).relinquish()),
+      ...this.networks.map((network) =>
+          this.mustGetBridge(network).relinquish(),
+      ),
+    ]);
+  }
+
   // Intended entrypoint.
   async deployAndRelinquish(): Promise<ethers.PopulatedTransaction[]> {
     // validate the config input
@@ -146,116 +278,6 @@ export default class DeployContext extends MultiProvider<config.Domain> {
     // combine governance transactions and return them
     governanceTransactions.push.apply(bridgeGovernanceTransactions);
     return governanceTransactions;
-  }
-
-  // Deploys all configured connections and enrolls them if possible.
-  // For any connection that cannot be enrolled, outputs the governance
-  // action required to enroll them
-  async ensureCoreConnections(): Promise<ethers.PopulatedTransaction[]> {
-    // ensure all core contracts are deployed
-    await this.ensureCores();
-    // ensure all core contracts are enrolled in each other
-    const enrollTransactions = await Promise.all(
-      this.networks.map(async (network) => {
-        const core = this.mustGetCore(network);
-        const name = this.resolveDomainName(network);
-        const remoteDomains = this.data.protocol.networks[name]?.connections;
-        // the following "unreachable" error performs type-narrowing for the compiler
-        if (!remoteDomains) throw new Error('unreachable');
-        const txns = await Promise.all(
-          remoteDomains.map((remote) => core.enrollRemote(remote)),
-        );
-        return txns.flat();
-      }),
-    );
-    return enrollTransactions.flat();
-  }
-
-  /// Deploys all configured Cores
-  async ensureCores(): Promise<void> {
-    const networksToDeploy = this.networks.filter((net) => !this.cores[net]);
-    await Promise.all(
-      networksToDeploy.map((net) =>
-        this.deployCore(this.mustGetDomainConfig(net)),
-      ),
-    );
-  }
-
-  protected async deployCore(domain: config.Domain): Promise<void> {
-    this.addDomain(domain);
-
-    const core = new CoreContracts(this, domain.name);
-    await core.recordStartBlock();
-
-    await Promise.all([
-      core.deployUpgradeBeaconController(),
-      core.deployUpdaterManager(),
-      core.deployXAppConnectionManager(),
-    ]);
-
-    await core.deployHome();
-    await core.deployGovernanceRouter();
-
-    // all contracts deployed
-    const complete = core.complete();
-    this.addCore(domain.name, complete);
-  }
-
-  async ensureBridgeConnections(): Promise<ethers.PopulatedTransaction[]> {
-    // first, ensure all bridge contracts are deployed
-    await this.ensureBridges();
-    // next, ensure all bridge contracts are enrolled in each other
-    // and all custom tokens are also enrolled
-    const enrollTransactions = await Promise.all(
-      this.networks.map(
-        async (network): Promise<ethers.PopulatedTransaction[]> => {
-          const bridge = this.mustGetBridge(network);
-          const name = this.resolveDomainName(network);
-          const remoteDomains = this.data.protocol.networks[name]?.connections;
-          // the following "unreachable" error performs type-narrowing for the compiler
-          if (!remoteDomains) throw new Error('unreachable');
-          const allEnrollRouterTxns = await Promise.all(
-            remoteDomains.map((remote) => bridge.enrollBridgeRouter(remote)),
-          );
-          const enrollTxns = allEnrollRouterTxns.flat();
-          // deploy and enroll custom tokens
-          const txns = await bridge.deployCustomTokens();
-          enrollTxns.push.apply(txns);
-          return enrollTxns;
-        },
-      ),
-    );
-    return enrollTransactions.flat();
-  }
-
-  /// Deploys all configured bridges.
-  async ensureBridges(): Promise<void> {
-    const toDeploy = this.networks.filter((net) => !this.bridges[net]);
-    await Promise.all(toDeploy.map((net) => this.deployBridge(net)));
-  }
-
-  protected async deployBridge(name: string): Promise<void> {
-    this.mustGetCore(name).complete(); // assert that the core is totally deployed
-
-    const bridge = new BridgeContracts(this, name);
-    await bridge.recordStartBlock();
-
-    await bridge.deployTokenUpgradeBeacon();
-    await bridge.deployTokenRegistry();
-    await bridge.deployBridgeRouter();
-    await bridge.deployEthHelper();
-
-    this.addBridge(name, bridge.complete());
-  }
-
-  async relinquishOwnership(): Promise<void> {
-    // relinquish deployer control
-    await Promise.all([
-      ...this.networks.map((network) => this.mustGetCore(network).relinquish()),
-      ...this.networks.map((network) =>
-        this.mustGetBridge(network).relinquish(),
-      ),
-    ]);
   }
 
   // perform validation checks on core and bridges
@@ -309,27 +331,5 @@ export default class DeployContext extends MultiProvider<config.Domain> {
       (contract) => contract.name == name,
     )[0].address;
     expect(utils.equalIds(inputAddr, addr));
-  }
-
-  pushVerification(
-    nameOrDomain: string | number,
-    verification: Verification,
-  ): void {
-    const name = this.resolveDomainName(nameOrDomain);
-    if (!this.verification.has(name)) this.verification.set(name, []);
-
-    const net = this._verification.get(name);
-    if (net) net.push(verification);
-  }
-
-  mustGetVerification(nameOrDomain: string | number): readonly Verification[] {
-    const domain = this.resolveDomainName(nameOrDomain);
-    const verification = this.verification.get(domain);
-    if (!verification)
-      throw new Error(
-        `Verification with name ${nameOrDomain} for domain ${domain} is not defined`,
-      );
-
-    return verification;
   }
 }
