287918fc12dbfdd89a3beae51a46de0ca216574c
Anna Carroll <anna.s.carroll@gmail.com>
2022-04-04 14:27:20 +0200

    log if checks pass

--- packages/new-deploy/scripts/check.ts
+++ packages/new-deploy/scripts/check.ts
@@ -2,9 +2,14 @@ import DeployContext from '../src/DeployContext';
 import * as config from '@nomad-xyz/configuration';
 import {getConfig} from "./utils";
 
-// instantiate deploy context
-const DEPLOY_CONFIG: config.NomadConfig = getConfig();
-const deployContext = new DeployContext(DEPLOY_CONFIG);
+run();
 
-// run checks on deployment
-deployContext.checkDeployment();
+async function run() {
+// instantiate deploy context
+    const DEPLOY_CONFIG: config.NomadConfig = getConfig();
+    const deployContext = new DeployContext(DEPLOY_CONFIG);
+
+    // run checks on deployment
+    await deployContext.checkDeployment();
+    console.log(`CHECKS PASS!`);
+}
