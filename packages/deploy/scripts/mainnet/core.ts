0d3f9be043606f70b3b04978da326b97d327648e
Imti Majeed <imtiazmaj@gmail.com>
2022-04-26 16:24:54 -0700

    chore: remove old deploy, rename new-deploy

--- packages/deploy/scripts/mainnet/core.ts
+++
@@ -1,9 +0,0 @@
-import { deployComplete } from '../../src/core';
-import * as ethereum from '../../config/mainnets/ethereum';
-import * as moonbeam from '../../config/mainnets/moonbeam';
-import { CoreDeploy } from '../../src/core/CoreDeploy';
-
-const ethereumDeploy = new CoreDeploy(ethereum.chain, ethereum.config);
-const moonbeamDeploy = new CoreDeploy(moonbeam.chain, moonbeam.config);
-
-deployComplete([ethereumDeploy, moonbeamDeploy]);
--- packages/deploy/src/index.ts
+++ packages/deploy/src/index.ts
@@ -1,14 +1,6 @@
-export * as core from './core';
-export * as bridge from './bridge';
-export * as incremental from './incremental';
+export * from './Contracts';
+export * from './bridge';
+export * from './core';
+export * from './utils';
 
-export * as deploy from './deploy';
-export * as chain from './chain';
-export * as contracts from './contracts';
-export * as proxyUtils from './proxyUtils';
-
-export * from './chain'
-export * from './deploy'
-export * from './core'
-export * from './incremental'
-export * from './bridge'
+export { DeployContext, Verification } from './DeployContext';
