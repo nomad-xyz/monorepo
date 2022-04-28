29853fbbddb44b1f8ab35cd664e561c6050184ef
Odysseas Lamtzidis <odyslam@gmail.com>
2022-04-28 15:18:43 +0200

    feat: homeDomain, onlyUpdaterManagersetUpdater

--- packages/contracts-core/contracts/test/Home.t.sol
+++ packages/contracts-core/contracts/test/Home.t.sol
@@ -2,13 +2,28 @@
 pragma solidity >=0.6.11;
 
 import {Home} from "../Home.sol";
-import {NomadTest} from "./utils/NomadTest.sol";
+import {NomadTestWithUpdaterManager} from "./utils/NomadTest.sol";
+import {IUpdaterManager} from "../interfaces/IUpdaterManager.sol";
 
-contract HomeTest is NomadTest {
+contract HomeTest is NomadTestWithUpdaterManager {
     Home home;
 
     function setUp() public override {
         super.setUp();
+        home = new Home(domain);
+        home.initialize(IUpdaterManager(address(updaterManager)));
+    }
+
+    function test_homeDomain() public {
+        assertEq(
+            keccak256(abi.encodePacked(domain, "NOMAD")),
+            home.homeDomainHash()
+        );
+    }
+
+    function test_onlyUpdaterManagerSetUpdater() public {
+        vm.prank(address(updaterManager));
+        home.setUpdater(vm.addr(420));
     }
 
     function dispatchGetRoot(
