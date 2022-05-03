405d7735c29b05bdfb688539ba860a55a96ec723
Daniil Naumetc <daniil.naumetc@gmail.com>
2022-05-03 18:49:57 +0200

    feature: new env

---
+++ packages/local-environment/src/neww/le.ts
@@ -0,0 +1,100 @@
+import { NomadLocator, NomadConfig } from "@nomad-xyz/configuration";
+import { DeployContext } from "../../../new-deploy/src/DeployContext";
+import { HardhatNetwork, Network } from "./network";
+import ethers from 'ethers';
+import { NonceManager } from "@ethersproject/experimental";
+
+
+class Env {
+    networks: Network[];
+    governor: NomadLocator;
+
+    constructor(governor: NomadLocator) {
+        this.networks = [];
+        this.governor = governor;
+    }
+
+    addNetwork(n: Network) {
+        if (!this.networks.includes(n)) this.networks.push(n);
+    }
+    
+
+    get govNetwork(): Network {
+        const n = this.networks.find(n => n.domainNumber === this.governor.domain);
+        if (!n) throw new Error(`Governing network is not present. GovDomain ${this.governor.domain}, present network domains: ${this.networks.map(n => n.domainNumber).join(', ')}`);
+        return n;
+    }
+
+    async deployFresh(): Promise<void> {
+
+    }
+
+    async deploy(): Promise<void> {
+        // if (this.deployedOnce()) {
+        //     this.deployIncrementally()
+        // } else {
+        //     this.deployFresh()
+        // }
+        const governanceBatch = await this.deployContext.deployAndRelinquish();
+        console.log(`Deployed! gov batch:`, governanceBatch);
+    }
+
+    // deployedOnce(): boolean {
+    //     return false;
+    // }
+
+    get deployerKey(): string {
+        return '1000000000000000000000000000000000000000000000000000000000000001';
+    }
+
+    get deployContext(): DeployContext {
+        const deployContext = new DeployContext(this.nomadConfig());
+        // add deploy signer and overrides for each network
+        for (const network of this.networks) {
+            const name = network.name;
+            const provider = deployContext.mustGetProvider(name);
+            const wallet = new ethers.Wallet(this.deployerKey, provider);
+            const signer = new NonceManager(wallet);
+            deployContext.registerSigner(name, signer);
+            deployContext.overrides.set(name, network.deployOverrides);
+        }
+        return deployContext;
+    }
+    
+
+    nomadConfig(): NomadConfig {
+        return {
+            version: 0,
+            environment: 'local',
+            networks: this.networks.map(n => n.name),
+            rpcs: Object.fromEntries(this.networks.map(n => [n.name, n.rpcs])),
+            protocol: {governor: this.governor, networks: Object.fromEntries(this.networks.map(n => [n.name, n.domain]))},
+            core: Object.fromEntries(this.networks.filter(n => n.isDeployed).map(n => [n.name, n.coreContracts!])),
+            bridge: Object.fromEntries(this.networks.filter(n => n.isDeployed).map(n => [n.name, n.bridgeContracts!])),
+            agent: Object.fromEntries(this.networks.filter(n => n.isDeployed).map(n => [n.name, n.agentConfig!])),
+            bridgeGui: Object.fromEntries(this.networks.filter(n => n.isDeployed).map(n => [n.name, n.bridgeGui!])),
+        }
+    }
+}
+
+(async () => {
+    const t = new HardhatNetwork('tom', 1);
+    await t.up();
+
+    const j = new HardhatNetwork('jerry', 2);
+    await j.up();
+
+    const le = new Env({domain: t.domainNumber, id: '0x'+'00'.repeat(20)});
+    le.addNetwork(t);
+    le.addNetwork(j);
+
+    await le.deploy();
+
+    // let myContracts = le.deploymyproject();
+
+
+    
+})()
+
+
+
---
+++ packages/local-environment/src/neww/network.ts
@@ -0,0 +1,228 @@
+import { Agents } from "./agent";
+import { NetworkSpecs, ContractConfig, BridgeConfiguration, Domain, CoreContracts, BridgeContracts, AgentConfig, AppConfig } from '@nomad-xyz/configuration';
+import { DockerizedActor } from "./actor";
+import Dockerode from "dockerode";
+import { sleep } from "../utils";
+import ethers from 'ethers';
+import {  } from "@nomad-xyz/configuration";
+
+enum DockerNetworkStatus {
+    Running,
+    Stopped,
+    Disconnected,
+    Waiting, // Transition period
+  }
+
+export abstract class Network {
+    agents?: Agents;
+
+    connectedNetworks: Network[];
+
+    name: string;
+    domainNumber: number;
+    chainId: number;
+    deployOverrides: ethers.Overrides;
+
+    coreContracts?: CoreContracts;
+    bridgeContracts?: BridgeContracts;
+    agentConfig?: AgentConfig;
+    bridgeGui?: AppConfig;
+
+
+    abstract get specs(): NetworkSpecs;
+    abstract get rpcs(): string[];
+    abstract get config(): ContractConfig;
+    abstract get bridgeConfig(): BridgeConfiguration;
+
+    abstract up(): Promise<void>;
+    abstract down(): Promise<void>;
+    abstract isConnected(): Promise<boolean>;
+
+    constructor(name: string, domainNumber: number, chainId: number) {
+        this.connectedNetworks = [];
+        this.name = name;
+        this.domainNumber = domainNumber;
+        this.chainId = chainId;
+
+        this.deployOverrides = {};
+    }
+
+    get connections(): string[] {
+        return this.connectedNetworks.map(n => n.name);
+    }
+
+    get domain(): Domain {
+        return {
+            name: this.name,
+            domain: this.domainNumber,
+            connections: this.connections,
+            specs: this.specs,
+            configuration: this.config,
+            bridgeConfiguration: this.bridgeConfig,
+        }
+    }
+
+    get isDeployed(): boolean {
+      return !!this.coreContracts && !!this.bridgeContracts
+    }
+
+}
+
+
+export class DockerizedNetworkActor extends DockerizedActor {
+    port: number;
+    blockTime: number;
+
+    constructor(name: string) {
+        super(name, 'network');
+        this.port = 1337;
+        this.blockTime = 1*1000;
+    }
+
+    async createContainer(): Promise<Dockerode.Container> {
+        const name = this.containerName();
+    
+        return this.docker.createContainer({
+          Image: "hardhat",
+          name,
+          Env: [
+            `BLOCK_TIME=${this.blockTime}`,
+            // ...this.keys.map((k, i) => `PRIVATE_KEY${i + 1}=${k.toString()}`),
+          ],
+          ExposedPorts: {
+            "8545/tcp:": {},
+          },
+          HostConfig: {
+            PortBindings: {
+              "8545/tcp": [
+                {
+                  HostPort: this.port.toString(),
+                },
+              ],
+            },
+            // NetworkMode: 'host',
+            // AutoRemove: true,
+          },
+        });
+      }
+    
+      async status(): Promise<DockerNetworkStatus> {
+        if (await this.isConnected()) {
+          if (await this.isRunning()) {
+            return DockerNetworkStatus.Running;
+          } else {
+            return DockerNetworkStatus.Stopped;
+          }
+        } else {
+          return DockerNetworkStatus.Disconnected;
+        }
+      }
+
+}
+
+
+export class HardhatNetwork extends Network {
+    firstStart: boolean;
+    blockTime: number;
+    handler: DockerizedNetworkActor;
+
+    updater: string;
+    watcher: string;
+    recoveryManager: string;
+    weth: string;
+
+    constructor(name: string, domain: number) {
+        super(name, domain, domain);
+        this.handler = new DockerizedNetworkActor(this.name+'xxx');
+        this.blockTime = 5;
+        this.firstStart = false;
+
+        this.updater = '';
+        this.watcher = '';
+        this.recoveryManager = '';
+        this.weth = '';
+    }
+
+    get connections(): string[] {
+        return this.connectedNetworks.map(n => n.name);
+    }
+
+    get rpcs(): string[] {
+        return [];
+    }
+
+    get specs(): NetworkSpecs {
+        return {
+            chainId: this.chainId,
+            finalizationBlocks: 2,
+            blockTime: this.blockTime,
+            supports1559: true,
+            confirmations: 2,
+            blockExplorer: '',
+            indexPageSize: 2000,
+          }
+    }
+    get config(): ContractConfig {
+        return {
+            optimisticSeconds: 18,
+            processGas: 850000,
+            reserveGas: 25000,
+            maximumGas: 1000000,
+            governance: {
+                recoveryManager: this.recoveryManager,
+                recoveryTimelock: 86400
+            },
+            updater: this.updater,
+            watchers: [this.watcher]
+          }
+    }
+    get bridgeConfig(): BridgeConfiguration {
+        return {
+            weth: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2KEK",
+            customs: [],
+            mintGas: 200000,
+            deployGas: 850000
+          }
+    }
+
+    private async connect() {
+        this.firstStart = await this.handler.connect();
+    }
+
+    private async disconnect() {
+        await this.handler.disconnect();
+    }
+
+
+
+    private async start() {
+        if ((await this.handler.status()) === DockerNetworkStatus.Running) return;
+        await this.handler.start();
+
+        if (this.firstStart) {
+            await sleep(20_000);
+            this.firstStart = false;
+        }
+    }
+
+    private async stop() {
+        await this.handler.stop();
+    }
+
+    async isConnected(): Promise<boolean> {
+        return this.handler.isConnected()
+    }
+
+    async up() {
+        await this.connect();
+        await this.start();
+    }
+
+    async down() {
+        await this.connect();
+
+        await this.stop();
+        await this.disconnect();
+    }
+
+}
