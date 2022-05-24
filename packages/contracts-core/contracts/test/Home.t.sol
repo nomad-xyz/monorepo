22fe583c4723674ef43c6659a8c565ce7ea89b22
Odysseas.eth <odyslam@gmail.com>
2022-05-23 20:55:11 -0700

    fix: set home to updateManager

--- packages/contracts-core/contracts/test/Home.t.sol
+++ packages/contracts-core/contracts/test/Home.t.sol
@@ -4,6 +4,8 @@ pragma solidity >=0.6.11;
 import {Home} from "../Home.sol";
 import {NomadTestWithUpdaterManager} from "./utils/NomadTest.sol";
 import {IUpdaterManager} from "../interfaces/IUpdaterManager.sol";
+import {Message} from "../libs/Message.sol";
+
 
 contract HomeTest is NomadTestWithUpdaterManager {
     Home home;
@@ -12,6 +14,7 @@ contract HomeTest is NomadTestWithUpdaterManager {
         super.setUp();
         home = new Home(homeDomain);
         home.initialize(IUpdaterManager(address(updaterManager)));
+        updaterManager.setHome(address(home));
     }
 
     function test_homeDomain() public {
@@ -26,6 +29,11 @@ contract HomeTest is NomadTestWithUpdaterManager {
         home.setUpdater(vm.addr(420));
     }
 
+    function test_committedRoot() public{
+      bytes32 emptyRoot;
+      assertEq(abi.encode(home.committedRoot()), abi.encode(emptyRoot));
+    }
+
     event Dispatch(
         bytes32 indexed messageHash,
         uint256 indexed leafIndex,
@@ -34,16 +42,55 @@ contract HomeTest is NomadTestWithUpdaterManager {
         bytes message
     );
 
-    // TODO: Fuzzing test
-//    function test_dispatchMessage() public {
-//        uint32 destDomain = 420;
-//        bytes32 destAddr = "0xFe8874778f946Ac2990A29eba3CFd50760593B2F";
-//        bytes memory message = bytes("message");
-//        vm.expectEmit(true, true, true, true);
-//        emit Dispatch(
-//        home.dispatch(destDomain, desAddr, message);
-//
-//    }
+    function test_dispatch() public {
+        bytes32 recipient = bytes32(uint256(uint160(vm.addr(1505))));
+        address sender = vm.addr(1555);
+        bytes memory messageBody = bytes("hey buddy");
+        uint32 nonce = home.nonces(remoteDomain);
+        bytes memory message = Message.formatMessage(
+          homeDomain,
+          bytes32(uint256(uint160(sender))),
+          nonce,
+          remoteDomain,
+          recipient,
+          messageBody
+        );
+        bytes32 messageHash = keccak256(message);
+        vm.expectEmit(true, true, true, true);
+        emit Dispatch(messageHash, 0,(uint64(remoteDomain) << 32) | nonce, home.committedRoot(), message);
+        vm.prank(sender);
+        home.dispatch(remoteDomain, recipient, messageBody);
+        assert(home.queueContains(home.root()));
+    }
+
+    function test_dispatchRejectBigMessage() public {
+        bytes32 recipient = bytes32(uint256(uint160(vm.addr(1505))));
+        address sender = vm.addr(1555);
+        bytes memory messageBody = new bytes(2 * 2**10 + 1);
+        uint32 nonce = home.nonces(remoteDomain);
+        bytes memory message = Message.formatMessage(
+          homeDomain,
+          bytes32(uint256(uint160(sender))),
+          nonce,
+          remoteDomain,
+          recipient,
+          messageBody
+        );
+        vm.prank(sender);
+        vm.expectRevert("msg too long");
+        home.dispatch(remoteDomain, recipient, messageBody);
+    }
+
+    event ImproperUpdate(bytes32 oldRoot, bytes32 newRoot, bytes signature);
+
+    function test_improperUpdate() public {
+        bytes32 newRoot = "new root";
+        bytes32 oldRoot = home.committedRoot();
+        bytes memory sig = signHomeUpdate(updaterPK, oldRoot, newRoot);
+        vm.expectEmit(false, false, false, true);
+        emit ImproperUpdate(oldRoot, newRoot, sig);
+        home.improperUpdate(oldRoot, newRoot, sig);
+    }
 
     function dispatchGetRoot(
         uint32 domain,
