0d00b4ed0c60a07c97d0988205427b86e8e75626
Anna Carroll <anna.s.carroll@gmail.com>
2022-05-23 23:21:35 -0700

    dedup naming Q

--- packages/contracts-core/contracts/interfaces/IMessageRecipient.sol
+++ packages/contracts-core/contracts/interfaces/IMessageRecipient.sol
@@ -16,10 +16,9 @@ interface IMessageRecipient {
 // to get finer control over Replica behavior:
 // - block message processing if Fraud has been proven
 // - specify gas requested to process a message
+// TODO: naming?!?!?!
 interface IPreflight {
-    // TODO: naming?!?!?!
     function preflight(
-        // TODO: naming?!??!!
         uint32 _origin,
         uint32 _nonce,
         bytes32 _sender,
