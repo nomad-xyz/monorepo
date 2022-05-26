074afe2c9d373529956ffce9dc982b0bd8f14f1e
Conner Swann <2635475+yourbuddyconner@users.noreply.github.com>
2022-05-26 16:04:54 -0700

    made setDetails indexer-aware

--- packages/monitor/src/setDetails.ts
+++ packages/monitor/src/setDetails.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 import { ethers } from 'ethers';
 import { BridgeToken__factory } from '@nomad-xyz/contract-interfaces/bridge';
+import axios from 'axios';
 
 // TODO: move to SDK
 interface TokenDetails {
@@ -10,36 +11,52 @@ interface TokenDetails {
   decimals: number;
 }
 
-// TODO: move to common file
-function getRpcProviderFromNetwork(
-  network: string,
-): ethers.providers.JsonRpcProvider {
-  let rpcUrl: string;
-  switch (network) {
-    case 'ethereum':
-      rpcUrl = process.env.ETHEREUM_RPC!;
-      break;
-    case 'moonbeam':
-      rpcUrl = process.env.MOONBEAM_RPC!;
-      break;
-    case 'moonbasealpha':
-      rpcUrl = process.env.MOONBASEALPHA_RPC!;
-      break;
-    default:
-      throw new Error(`No RPC url for network ${network}`);
-  }
+async function getConfig(environment: string): Promise<any>{
+  const configUrl = `https://raw.githubusercontent.com/nomad-xyz/rust/b76315abcc5385987f3e71f75d40909dd24b95c3/configuration/configs/${environment}.json`
+  const res = await axios(configUrl);
+  const config = await res.data
+  return config
+}
 
+// TODO: move to common file
+async function getRpcProviderFromConfig(
+  environment: string,
+  network: string
+): Promise<ethers.providers.JsonRpcProvider> {
+  const config = await getConfig(environment)
+  let rpcUrl: string;
+  try {
+    rpcUrl = config.rpcs[network][0];
+  }
+  catch(e){
+    throw new Error(`No RPC url for network ${network}`);
+  }
   return new ethers.providers.JsonRpcProvider(rpcUrl);
 }
 
-function getSigner(network: string): ethers.Signer {
+async function getSigner(environment: string, network: string): Promise<ethers.Signer> {
   const privKey = process.env.SET_DETAILS_KEY!;
-  const provider = getRpcProviderFromNetwork(network);
+  const provider = await getRpcProviderFromConfig(environment, network);
   return new ethers.Wallet(privKey, provider);
 }
 
+async function getNetworkName(environment: string, domain: number) {
+  const config = await getConfig(environment)
+  const networkNames: number[] = config.networks
+  let lookup: any = {
+    "names": {},
+    "domains": {}
+  }
+  networkNames.forEach(element => {
+    const domain = config.protocol.networks[element].domain
+    lookup.names[element] = domain.toString()
+    lookup.domains[domain] = element.toString()
+  });
+  return lookup.domains[domain]
+
+}
+
 async function setDetailsForToken(
-  network: string,
   signer: ethers.Signer,
   details: TokenDetails,
 ) {
@@ -47,14 +64,13 @@ async function setDetailsForToken(
 
   const token = BridgeToken__factory.connect(address, signer);
 
-  console.log('Calling set details on contract...');
+  console.log(`Calling set details on ${symbol} contract ${address}...`);
   const tx = await token.setDetails(name, symbol, decimals);
 
   console.log('Waiting for 3 confirmations...');
   await tx.wait(3);
 
   console.log('Successfully set details for token!');
-  console.log(`- network: ${network}`);
   console.log(`- address: ${address}`);
   console.log(`- name: ${name}`);
   console.log(`- symbol: ${symbol}`);
@@ -65,25 +81,30 @@ async function setDetailsForToken(
 
 /* Usage:
  * 1. set SET_DETAILS_KEY in .env file (as well as RPC urls)
- * 2. npm run set-details <network> <token_address> <token_name> <symbol> <decimals>
+ * 2. yarn run set-details <environment>
  */
 (async () => {
   dotenv.config({ path: process.env.CONFIG_PATH ?? '.env' });
 
   const args = process.argv.slice(2);
-  const network = args[0];
-  const address = args[1];
-  const name = args[2];
-  const symbol = args[3];
-  const decimals = parseInt(args[4]);
+  const environment = args[0];
 
-  const signer = getSigner(network);
-  const details = {
-    address,
-    name,
-    symbol,
-    decimals,
-  };
+  const url = `https://bridge-indexer.${environment=="development" ? "dev"  : environment=="staging" ? "staging" : environment=="production" ? "prod" : "prod"}.madlads.tools/wrongReplicas`
+  const res = await axios(url)
+  let wrongReplicas = res.data as any[]
 
-  await setDetailsForToken(network, signer, details);
+  for (let replica of wrongReplicas){
+   
+    const domain: number = replica["domain"]
+    const networkName = await getNetworkName(environment, domain)
+    const signer = await getSigner(environment, networkName);
+    const details = {
+      address: replica.id,
+      name: replica.token['name'],
+      symbol: replica.token['symbol'],
+      decimals: replica.token['decimals']
+    };
+    console.log(`Setting details for ${replica.token["name"]} on ${networkName}`)
+    await setDetailsForToken(signer, details);
+  }
 })();
