7ed5f80991b2c4710a7e3dfa3be83fc6ed5961f1
Arnaud Delabarre <delabarre.arnaud@gmail.com>
2022-05-31 12:08:12 -0700

    Formating amount to int instead of scientific notation

--- packages/keymaster/src/utils.py
+++ packages/keymaster/src/utils.py
@@ -53,7 +53,8 @@ def create_transaction(sender_key:str, recipient_address:int, amount:int, nonce:
         data=b'',
         chainId=chain_id,
     )
-    signed_txn = w3.eth.account.sign_transaction(tx_params,sender_key)
+
+    signed_txn = w3.eth.account.sign_transaction(tx_params, sender_key)
     return (tx_params, signed_txn)
 
 
@@ -107,7 +108,7 @@ def check_account(home_network: str, target_network: str, role: str, address: st
     if role != "bank" and wallet_wei < (threshold / 4): 
         logger.debug(f"Balance is low for {home_network} {role} ({address}) on {target_network} - {wallet_wei * 10**-18 } < {threshold * 10**-18 / 4}")
         should_top_up = True
-        top_up_amount = threshold - wallet_wei
+        top_up_amount = int(threshold - wallet_wei)
     # Warn when the bank is ~4 top-ups from being empty
     if role == "bank" and wallet_wei < threshold * 4:
         logger.warning(f"Bank balance is low for {home_network} ({address}) - {wallet_wei * 10**-18} < {threshold * 4 * 10**-18}")
