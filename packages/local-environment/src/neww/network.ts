4f13ec22cb98b308223429ba897371aa626775b9
Xiangan He <xiangan9@gmail.com>
2022-06-03 11:13:25 -0400

    rebase

--- packages/local-environment/src/neww/network.ts
+++ packages/local-environment/src/neww/network.ts
@@ -256,7 +256,7 @@ export class HardhatNetwork extends Network {
     }
     get bridgeConfig(): BridgeConfiguration {
         return {
-            weth: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2KEK",
+            weth: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
             customs: [],
             mintGas: 200000,
             deployGas: 850000
