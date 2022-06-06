d26f8dff6c202f4a3c7c00b6fddce14e366597ad
Conner Swann <2635475+yourbuddyconner@users.noreply.github.com>
2022-06-06 13:40:02 -0700

    reworked test structure -- works

--- packages/indexer/load-tests/transaction-history.ts
+++ packages/indexer/load-tests/transaction-history.ts
@@ -1,105 +1,36 @@
-import { step, TestSettings, beforeAll} from '@flood/element'
-import { ApolloClient, gql, InMemoryCache, HttpLink } from '@apollo/client';
-import fetch from 'cross-fetch';
+import { step, TestSettings, beforeAll, TestData} from '@flood/element'
+import { buildUri, getAddresses, getMessageHistory } from './utils';
 
 export const settings: TestSettings = {
 	userAgent: 'flood-load-test',
-	loopCount: 50,
-    actionDelay: 0.5,
+	loopCount: 5,
+    actionDelay: 0.1,
+    duration: -1,
     stages: [
-        { duration: '1m', target: 5 },
-        { duration: '5m', target: 30 },
-        { duration: '1m', target: 5 },
+        { duration: '10m', target: 1 },
+        { duration: '10m', target: 5 },
+        { duration: '30m', target: 10 },
+        { duration: '30m', target: 20 },
+        { duration: '30m', target: 30 },
+        { duration: '30m', target: 20 },
+        { duration: '30m', target: 10 },
+        { duration: '10m', target: 5 },
+        { duration: '10m', target: 1 },
     ]
 }
 
-
-let myClient: any;
-let addresses = [
-    "0xF5e6CC7FA0bf3c96B3def2863eC0dC03ce5DC737",
-    "0x9791c9dF02D34F2e7d7322D655535d9849E8da5c",
-    "0x952318478309f711268796d89544E6421C0fC12A"
-]
-
+const ENVIRONMENT = process.env["ENVIRONMENT"] || "development"
+const indexerURI = buildUri(ENVIRONMENT)
+let addresses = [];
 
 export default () => {
-    beforeAll(async browser => {
-		// Run this hook before running the first step
-		myClient = new ApolloClient({
-            link: new HttpLink({
-                uri: 'https://bridge-indexer.dev.madlads.tools/graphql',
-                fetch: fetch
-            }),
-            cache: new InMemoryCache() 
-        });
-	})
+    beforeAll(async () => {
+      addresses = await getAddresses(indexerURI);
+	  })
     step("Query Tx History", async () => {
-        const address = addresses[Math.floor(Math.random() * addresses.length)]
-
-        const GET_TRANSACTION_HISTORY = gql`
-          query Query($where: MessagesWhereInput) {
-            findManyMessages(where: $where, take: 10, skip: 0) {
-              id
-              messageHash
-              origin
-              destination
-              nonce
-              internalSender
-              internalRecipient
-              msgType
-              root
-              state
-              dispatchBlock
-              dispatchedAt
-              updatedAt
-              relayedAt
-              receivedAt
-              processedAt
-              confirmAt
-              sender
-              recipient
-              amount
-              allowFast
-              detailsHash
-              tokenDomain
-              tokenId
-              body
-              leafIndex
-              tx
-              gasAtDispatch
-              gasAtUpdate
-              gasAtRelay
-              gasAtReceive
-              gasAtProcess
-              sent
-              updated
-              relayed
-              received
-              processed
-              createdAt
-            }
-          }
-        `;
-        
-        const HISTORY_VARS = {
-            "where": {
-              "OR": [
-                {
-                  "recipient": {
-                    "equals": `${address}`
-                  },
-                  "sender": {
-                    "equals": `${address}`
-                  }
-                }
-              ]
-            }
-        }
-        const {loading, data} = await myClient.query({
-            query: GET_TRANSACTION_HISTORY,
-            fetchPolicy: "no-cache" 
-        });
-        console.log(`Address: ${address}`)
-        console.log(`Number Messages: ${data.findManyMessages.length}`)
+      for (let index = 0; index < addresses.length; index++) {
+        const address = addresses[index];
+        const data = await getMessageHistory(indexerURI, address)
+      }
     })
 };
---
+++ packages/indexer/load-tests/utils.ts
@@ -0,0 +1,119 @@
+import { ApolloClient, gql, InMemoryCache, HttpLink } from '@apollo/client';
+import fetch from 'cross-fetch';
+
+export function buildUri(environment: string){
+    switch (environment) {
+      case "staging":
+        return 'https://bridge-indexer.staging.madlads.tools/graphql'
+      case "production":
+        return 'https://bridge-indexer.prod.madlads.tools/graphql'
+      default:
+        return 'https://bridge-indexer.dev.madlads.tools/graphql'
+    }
+}
+
+export function getClient(uri: string){
+    const client = new ApolloClient({
+        link: new HttpLink({
+          uri: uri,
+          fetch: fetch
+        }),
+        cache: new InMemoryCache() 
+    });
+    return client;
+}
+
+
+export function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+export async function getAddresses(uri: string) {
+    const client = getClient(uri);
+    const GET_ADDRESSES_QUERY = gql`
+        query FindManyMessages($where: MessagesWhereInput, $take: Int) {
+        findManyMessages(where: $where, take: $take) {
+            sender
+        }
+        }
+    `;
+
+    const GET_ADDRESSES_VARS = {
+        "where": {
+          "sender": {
+              "not": null
+          }
+        },
+        "take": 800
+    }
+    const {loading, data} = await client.query({
+        query: GET_ADDRESSES_QUERY,
+        variables: GET_ADDRESSES_VARS,
+        fetchPolicy: "no-cache" 
+    });
+
+    const addresses = data.findManyMessages.map(entry => entry.sender).filter(onlyUnique)
+    return addresses
+}
+
+export async function getMessageHistory(uri: string, address: string){
+    const client = getClient(uri);
+    const GET_TRANSACTION_HISTORY = gql`
+      query Query($where: MessagesWhereInput) {
+        findManyMessages(where: $where, take: 10, skip: 0) {
+          id
+          messageHash
+          origin
+          destination
+          nonce
+          internalSender
+          internalRecipient
+          msgType
+          root
+          state
+          dispatchBlock
+          dispatchedAt
+          updatedAt
+          relayedAt
+          receivedAt
+          processedAt
+          confirmAt
+          sender
+          recipient
+          amount
+          allowFast
+          detailsHash
+          tokenDomain
+          tokenId
+          body
+          leafIndex
+          tx
+          gasAtDispatch
+          gasAtUpdate
+          gasAtRelay
+          gasAtReceive
+          gasAtProcess
+          sent
+          updated
+          relayed
+          received
+          processed
+          createdAt
+        }
+      }
+    `;
+        
+    const HISTORY_VARS = {
+      "where": {
+        "sender": {
+          "equals": `${address}`
+        }
+      }
+    }
+    const {loading, data} = await client.query({
+        query: GET_TRANSACTION_HISTORY,
+        variables: HISTORY_VARS,
+        fetchPolicy: "no-cache" 
+    });
+    return data;
+}
