612b8c2d81863c0ff178bd0a0f90257a1d1f1772
ErinHales <mcbridee093@gmail.com>
2022-06-13 14:08:29 -0600

    add: bare bones sdk

---
+++ packages/sdk-governance/tests/index.test.ts
@@ -0,0 +1,17 @@
+import { describe, it } from "mocha";
+
+import { expect } from "chai";
+
+describe("sdk-govern", async () => {
+  // before(async () => {
+  //
+  // });
+
+  // beforeEach(async () => {
+  //
+  // });
+
+  it.skip("TODO", async () => {
+    expect(true).to.be.true;
+  });
+});
