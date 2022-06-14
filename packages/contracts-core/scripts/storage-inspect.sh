b5327cb110c39445b6493a1ae5be871d9725d5a6
Odysseas.eth <odyslam@gmail.com>
2022-06-14 11:40:59 +0300

    feat: forge-inspect all contracts

---
+++ packages/contracts-core/scripts/storage-inspect.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+if ! command -v forge &> /dev/null
+then
+    echo "forge could not be found. Please install forge by running:"
+    echo "curl -L https://foundry.paradigm.xyz | bash"
+    exit
+fi
+
+contracts=(
+Home
+Replica
+UpdaterManager
+XAppConnectionManager
+GovernanceRouter
+)
+
+dir=$(dirname "$0")
+
+echo "Creating storage layout diagrams.."
+
+echo "=======================" > $dir/../.storage-layout
+echo "👁👁 STORAGE LAYOUT snapshot 👁👁" >> $dir/../.storage-layout
+echo "=======================" >> $dir/../.storage-layout
+echo -e "\nCurrent git commit $(git rev-parse HEAD) \n\n" >> $dir/../.storage-layout
+
+for contract in ${contracts[@]}
+do
+  echo -e "\n=======================" >> $dir/../.storage-layout
+  echo "➡ $contract">> $dir/../.storage-layout
+  echo -e "=======================\n" >> $dir/../.storage-layout
+  FOUNDRY_PROFILE=core forge inspect --pretty $contract storage-layout >> $dir/../.storage-layout
+done
+echo "Storage layout snapshot stored at $dir/../.storage-layout"
