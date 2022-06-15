8b1ba6e0ee0c26c569902cac476935e7b995f6f6
ErinHales <mcbridee093@gmail.com>
2022-06-15 14:16:52 -0600

    update: sloppy encode function

--- packages/sdk-governance/src/index.ts
+++ packages/sdk-governance/src/index.ts
@@ -1,5 +1,8 @@
 import { NomadContext } from '@nomad-xyz/sdk';
 import * as config from '@nomad-xyz/configuration';
+import { utils, ethers } from 'ethers'
+import NomadModule from './abis/NomadModule.json';
+const { abi: NomadModuleABI } = NomadModule;
 
 type Address = string;
 type Domain = string | number;
@@ -12,6 +15,7 @@ type Domain = string | number;
  */
 export class GovernanceContext extends NomadContext {
   // private bridges: Map<string, BridgeContracts>;
+  private governorModule: Address;
   private govModules: Map<string, Address>;
 
   constructor(environment: string | config.NomadConfig = 'development') {
@@ -19,6 +23,7 @@ export class GovernanceContext extends NomadContext {
 
     this.govModules = new Map();
     // TODO: pass into constructor
+    this.governorModule = '0x0000'
     this.govModules.set('ethereum', '0x0000');
     this.govModules.set('moonbeam', '0x0000');
   }
@@ -43,7 +48,7 @@ export class GovernanceContext extends NomadContext {
    * @param nameOrDomain A domain name or number.
    * @returns the module address (or undefined)
    */
-  getGovModule(nameOrDomain: Domain): Address | undefined {
+  getGovModuleAddr(nameOrDomain: Domain): Address | undefined {
     const domain = this.resolveDomainName(nameOrDomain);
     return this.govModules.get(domain);
   }
@@ -54,12 +59,39 @@ export class GovernanceContext extends NomadContext {
    * @param nameOrDomain A domain name or number.
    * @returns the module address
    */
-  mustGetGovModule(nameOrDomain: Domain): Address {
-    const module = this.getGovModule(nameOrDomain);
+  mustGetGovModuleAddr(nameOrDomain: Domain): Address {
+    const module = this.getGovModuleAddr(nameOrDomain);
     if (!module) {
       throw new Error(`Missing governance module for domain: ${nameOrDomain}`);
     }
     return module;
+  }
+
+  getGovModule(nameOrDomain: Domain): ethers.Contract {
+    const addr = this.mustGetGovModuleAddr(nameOrDomain);
+    return new ethers.Contract(addr, NomadModuleABI);
+  }
+
+  async encodeProposalData(): Promise<string> {
+    // TODO: pass in props
+    const destination = 'ethereum';
+    const to: Address = '0x0000000000000000000000000000000000000000'
+    const value = 1
+    const data = '0x1234'
+    const module = this.getGovModule(destination);
+    const execTx = await module.populateTransaction.exec(
+      to,
+      value,
+      data
+    );
+    const encodedExec = utils.serializeTransaction(execTx);
+
+    // TODO: encode dispatch
+    return encodedExec
+  }
+
+  decodeProposalData() {
+    return
   }
 }
 
---
+++ packages/sdk-governance/src/types.ts
@@ -0,0 +1,8 @@
+type GovernanceConfig = {
+
+}
+
+const governanceConfig = {
+  ethereum: '0x00',
+  moonbeam: '0x00',
+}
