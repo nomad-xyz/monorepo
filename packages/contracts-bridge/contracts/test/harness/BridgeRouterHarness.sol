935a69bd664d39a93ce03fd6120ee984bbcbfb1a
Odysseas.eth <odyslam@gmail.com>
2022-07-12 12:24:34 +0100

    feat: add bridgeRouterHarness

---
+++ packages/contracts-bridge/contracts/test/harness/BridgeRouterHarness.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity 0.7.6;
+
+import {BridgeRouter} from "../../BridgeRouter.sol";
+
+contract BridgeRouterHarness is BridgeRouter {
+    function debitTokens(address token, uint256 amount)
+        external
+        returns (bytes29 _tokenId, bytes32 _detailsHash)
+    {
+        return _debitTokens(token, amount);
+    }
+
+    function sendTransferMessage(
+        uint32 dest,
+        bytes29 tokenId,
+        bytes29 action
+    ) external {
+        _sendTransferMessage(dest, tokenId, action);
+    }
+}
--- packages/contracts-bridge/contracts/test/utils/BridgeTest.sol
+++ packages/contracts-bridge/contracts/test/utils/BridgeTest.sol
@@ -1,43 +1,69 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity 0.7.6;
 
-// Bridge
-import {BridgeRouter} from "../../BridgeRouter.sol";
+// Test Contracts
+
+import {MockHome} from "./MockHome.sol";
+
+// Bridge Contracts
+import {BridgeRouterHarness} from "../harness/BridgeRouterHarness.sol";
 import {TokenRegistry} from "../../TokenRegistry.sol";
 import {BridgeToken} from "../../BridgeToken.sol";
-//
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+
+// Upgrade Contracts
 import {UpgradeBeacon} from "@nomad-xyz/contracts-core/contracts/upgrade/UpgradeBeacon.sol";
 import {UpgradeBeaconController} from "@nomad-xyz/contracts-core/contracts/upgrade/UpgradeBeaconController.sol";
 
+// Contract-core Contracts
 import {XAppConnectionManager} from "@nomad-xyz/contracts-core/contracts/XAppConnectionManager.sol";
 
 import "forge-std/Test.sol";
 import "forge-std/console2.sol";
 
 contract BridgeTest is Test {
+    // Local variables
     uint256 mockUpdaterPK;
     address mockUpdater;
+    address bridgeUser;
+    uint32 localDomain;
 
-    BridgeRouter bridgeRouter;
-    UpgradeBeacon tokenBeacon;
+    // Remote variables
+    bytes32 remoteBridgeRouter;
+    uint32 remoteDomain;
+
+    MockHome mockHome;
+    BridgeRouterHarness bridgeRouter;
     XAppConnectionManager xAppConnectionManager;
     UpgradeBeaconController upgradeBeaconController;
+    UpgradeBeacon tokenBeacon;
     // Token Registry for all tokens in the domain
     TokenRegistry tokenRegistry;
-    // Implementation contract for all tokens in the domain
     BridgeToken bridgeToken;
+    // Implementation contract for all tokens in the domain
+    ERC20Mock localToken;
 
     function setUp() public virtual {
+        // Mocks
+        // The Private Key numbers are random and of no significance. They
+        // serve as a seed for the cheatcode to generate a pseudorandom address.
         mockUpdaterPK = 420;
         mockUpdater = vm.addr(mockUpdaterPK);
+        bridgeUser = vm.addr(9305);
+        remoteBridgeRouter = addressToBytes32(vm.addr(99123));
+        uint256 initialAmount = 10000;
+
+        localToken = new ERC20Mock("Fake", "FK", bridgeUser, initialAmount);
+        localDomain = 1500;
+        remoteDomain = 3000;
+        mockHome = new MockHome(localDomain);
 
         // Create implementations
-        bridgeRouter = new BridgeRouter();
+        bridgeRouter = new BridgeRouterHarness();
         tokenRegistry = new TokenRegistry();
-        bridgeToken = new BridgeToken();
         xAppConnectionManager = new XAppConnectionManager();
         upgradeBeaconController = new UpgradeBeaconController();
-
+        bridgeToken = new BridgeToken();
         tokenBeacon = new UpgradeBeacon(
             address(bridgeToken),
             address(upgradeBeaconController)
@@ -54,6 +80,8 @@ contract BridgeTest is Test {
             address(tokenRegistry),
             address(xAppConnectionManager)
         );
+        xAppConnectionManager.setHome(address(mockHome));
+        bridgeRouter.enrollRemoteRouter(remoteDomain, remoteBridgeRouter);
     }
 
     function addressToBytes32(address addr) public returns (bytes32) {
