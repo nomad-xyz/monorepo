e2599729915014182250892af91e3cfeec070dcc
odysseas.eth <odyslam@gmail.com>
2022-07-15 09:49:13 +0100

    Testing: unit-tests/contracts-bridge (#387)

---
+++ packages/contracts-bridge/contracts/test/harness/TokenRegistryHarness.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity 0.7.6;
+
+import {TokenRegistry} from "../../TokenRegistry.sol";
+
+contract TokenRegistryHarness is TokenRegistry {
+    function deployToken(uint32 domain, bytes32 id)
+        external
+        returns (address _token)
+    {
+        return _deployToken(domain, id);
+    }
+}
