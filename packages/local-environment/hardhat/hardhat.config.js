f6f675774602dfa4c710ecd00d20030f85b71708
Xiangan He <xiangan9@gmail.com>
2022-07-25 11:40:38 -0400

    further feedback implementation

--- packages/local-environment/hardhat/hardhat.config.js
+++ packages/local-environment/hardhat/hardhat.config.js
@@ -1,5 +1,4 @@
 require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
