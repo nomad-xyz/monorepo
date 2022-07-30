0d30c8075884887f4fdcee7b3b6e4afca67959f3
Ryan Swanson <ryan@nomad.xyz>
2022-07-30 17:51:38 -0400

    forgot the src directory oops.

---
+++ packages/manual-processor/src/manuallyProcessMessages.ts
@@ -0,0 +1,122 @@
+import { request, gql } from "graphql-request";
+const { NomadContext } = await import('@nomad-xyz/sdk')
+
+const nomadAPI = "https://bridge-indexer.prod.madlads.tools/";
+
+export type IndexerTx = {
+  origin: number;
+  destination: number;
+  leafIndex: string;
+  sender: string;
+  state: number;
+  dispatchedAt: number;
+  updatedAt: number;
+  relayedAt: number;
+  processedAt: number;
+  receivedAt: number;
+  dispatchTx: string;
+  amount: string;
+  decimals: number;
+  tokenDomain: number;
+  tokenId: string;
+  confirmAt: number;
+};
+
+export async function getTransactionsForProcessing(
+  domains: number[],
+  address: string,
+  page: number,
+  states: number[],
+  size: number
+): Promise<Array<IndexerTx>> {
+  const skip = size * (page - 1);
+  const variables = JSON.stringify({
+    where: {
+      destination: {
+        in: domains,
+      },
+      origin: {
+        in: domains,
+      },
+      tokenDomain: {
+        in: domains,
+      },
+      state: {
+        in: states,
+      },
+    },
+  });
+  const query = gql`
+      query Query($where: MessagesWhereInput) {
+        findManyMessages(where: $where, take: ${size}, skip: ${skip}) {
+          id
+          messageHash
+          origin
+          destination
+          nonce
+          internalSender
+          internalRecipient
+          msgType
+          root
+          state
+          dispatchBlock
+          dispatchedAt
+          updatedAt
+          relayedAt
+          receivedAt
+          processedAt
+          confirmAt
+          sender
+          recipient
+          amount
+          allowFast
+          detailsHash
+          tokenDomain
+          tokenId
+          body
+          leafIndex
+          dispatchTx
+          gasAtDispatch
+          gasAtUpdate
+          gasAtRelay
+          gasAtReceive
+          gasAtProcess
+          sent
+          updated
+          relayed
+          received
+          processed
+          createdAt
+        }
+      }`;
+  return await request(`${nomadAPI}graphql`, query, variables).then(
+    (res) => res.findManyMessages
+  );
+}
+
+async function main() {
+  // This uses a helper function defined in ./registerRpcs.ts
+  // to register RPCs placed in environment variables
+  type Env = 'production' | 'staging' | 'development'
+  const environment: Env = 'production'
+  const nomadContext = await NomadContext.fetch(environment)
+
+
+  console.log(`Registered Domains: ${nomadContext.domainNames}`);
+
+  // const bridgeContext = BridgeContext.fromNomadContext(core);
+  const txArray = await getTransactionsForProcessing(
+    [0, 1], 
+    "moonbeam",
+    0,
+    [2, 3],
+    1
+  );
+  txArray.forEach((transaction) => {
+    nomadContext.processByOriginDestinationAndLeaf(transaction.origin, transaction.destination, transaction.leafIndex);
+  });
+}
+
+(async () => {
+  await main();
+})();
