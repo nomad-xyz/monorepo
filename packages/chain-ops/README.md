086ac531505a385f8a44819c4c782bebebd302b3
Erin Hales <36751902+ErinHales@users.noreply.github.com>
2022-08-10 15:27:20 -0600

    Erinhales/process single (#420)
    * refactor: rename package and script
    
    * chore: bump yarn.lock and update cache
    
    * install: delete and install from scratch
    
    * add: process-single script
    
    * install
    
    * update: version
    
    * fix: clear cache and re-install
    
    * fix: handle multiple transactions
    
    * chore: lint
    
    * chore: add missing dependency references
    
    * chore: remove skip lib check
    
    * fix: try running build with topological flag
    
    * chore: add topological flag for all yarn workspace foreach commands
    
    Co-authored-by: Imti Majeed <imtiazmaj@gmail.com>

--- packages/chain-ops/README.md
+++ packages/chain-ops/README.md
@@ -7,3 +7,9 @@ Environment for executing scripts that perform on-chain operations
 1) Set DEST_RPC_URL and signer PRIVATE_KEY in .env
 2) Set ORIGIN, DESTINATION, ENV, and PAGE_SIZE constants in `src/manual-process.ts`
 3) Run `yarn manual-process`
+
+## Process
+
+1) Set DEST_RPC_URL and signer PRIVATE_KEY in .env
+2) Set ORIGIN, DESTINATION, ENV, and TRANSACTION constants in `src/process.ts`
+3) Run `yarn process-single`
---
+++ packages/chain-ops/src/process.ts
@@ -0,0 +1,55 @@
+import { ethers } from 'ethers';
+import { NomadContext, NomadMessage } from '@nomad-xyz/sdk';
+
+type Env = 'production' | 'staging' | 'development';
+
+// user-defined script values
+const { PRIVATE_KEY, DEST_RPC_URL } = process.env;
+const ORIGIN = 'rinkeby';
+const DESTINATION = 'polygonmumbai';
+const TRANSACTION = '';
+const ENV: Env = 'staging';
+console.log(`${ENV}: ${ORIGIN} to ${DESTINATION}`);
+
+let nomadContext: NomadContext;
+
+// instanitiate BridgeContext, register provider and signer
+async function init(destination: string | number): Promise<void> {
+  nomadContext = await NomadContext.fetch(ENV);
+  if (DEST_RPC_URL) {
+    const destDomain = nomadContext.resolveDomain(destination);
+    nomadContext.registerRpcProvider(destDomain, DEST_RPC_URL);
+  }
+
+  // register signer
+  if (!PRIVATE_KEY) {
+    throw new Error('No private key configured');
+  }
+  const provider = nomadContext.getProvider(destination);
+  const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+  nomadContext.registerSigner(destination, wallet);
+  console.log('nomadContext initiated');
+}
+
+// loop over transactions in a batch and process each one, continue if there is an error
+async function processTx(
+  origin: string | number,
+  txHash: string,
+): Promise<void> {
+  console.log(`processing ${txHash}`);
+  const messages: NomadMessage<NomadContext>[] =
+    await NomadMessage.baseFromTransactionHash(nomadContext, origin, txHash);
+  for (let i = 0; i++; i < messages.length) {
+    const m = messages[i];
+    const processTx = await m.getProcess();
+    if (!processTx) {
+      await m.process();
+    }
+  }
+}
+
+async function start() {
+  await init(DESTINATION);
+  await processTx(ORIGIN, TRANSACTION);
+}
+start();
