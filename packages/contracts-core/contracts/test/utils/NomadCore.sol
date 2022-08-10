e693493f61eea67965391e623de2c0fd0fc0ee17
Odysseas.eth <odyslam@gmail.com>
2022-08-10 17:31:36 +0200

    chore: finish nomad core setup

--- packages/contracts-core/contracts/test/utils/NomadCore.sol
+++ packages/contracts-core/contracts/test/utils/NomadCore.sol
@@ -28,17 +28,27 @@ contract NomadCore is Test {
     Home homeImplementation;
     Replica replicaImplementation;
     XAppConnectionManager xAppConnectionManagerImplementation;
+    GovernanceRouter governanceRouterImplementation;
+    XAppConnectionManager xAppConnectionManagerImplementation;
 
     UpgradeBeaconProxy home;
     UpgradeBeaconProxy replica;
     UpgradeBeaconProxy updaterManager;
     UpgradeBeaconProxy governanceRouter;
     UpgradeBeaconProxy xAppConnectionManager;
+    UpgradeBeaconProxy xAppConnectionManager;
+
+    UpgradeBeacon homeBeacon;
+    UpgradeBeacon replicaBeacon;
+    UpgradeBeacon upgradeManagerBeacon;
+    UpgradeBeacon governanceRouterBeacon;
+    UpgradeBeacon xAppConnectionManagerBeacon;
 
     UpgradeBeaconController homeController;
     UpgradeBeaconController replicaController;
     UpgradeBeaconController updaterManagerController;
     UpgradeBeaconController governanceRouterController;
+    UpgradeBeaconController xAppConnectionManagerController;
 
     /*//////////////////////////////////////////////////////////////
                                  ACTORS
@@ -55,11 +65,24 @@ contract NomadCore is Test {
     uint256[] usersKeys;
     address[] users;
 
+    /*//////////////////////////////////////////////////////////////
+                                CONSTANTS
+    //////////////////////////////////////////////////////////////*/
+
+    uint32 homeDomain;
+    uint32 remoteDomain;
+
     function setUp() {
         xapp = new GoodXappSimple();
+        setUpActors();
+        createCore();
+        enrollWatcherToGovRouter();
+        setHome();
+        relinquishControl();
+        console2.log("Nomad Core is deployed!")
     }
 
-    function setUpActos() {
+    function setUpActors() {
         watcherKey = 1;
         watcher = vm.addr(watcherKey);
         updaterKey = 2;
@@ -68,6 +91,8 @@ contract NomadCore is Test {
         relayer = vm.addr(relayerKey);
         relayerKey = 4;
         relayer = vm.addr(processorKey);
+        recoveryAdminKey = 5;
+        recoveryAdmin = vm.addr(recoveryAdminKey);
 
         vm.label(updater, "Updater");
         vm.label(fakeUpdater, "fake Updater");
@@ -75,13 +100,107 @@ contract NomadCore is Test {
         vm.label(fakeWatcher, "fake Watcher");
         vm.label(processor, "Processor");
         vm.label(relayer, "Relayer");
+        vm.label(recoveryAdmin, "Recovery Admin");
+
+        console2.log("Updater   Address:", updater);
+        console2.log("Watcher   Address:", watcher);
+        console2.log("Relayer   Address:", relayer);
+        console2.log("Processor Address:", processor);
+        console2.log("Recovery Admin Address:", recoveryAdmin);
     }
 
-    function getEnv() {}
+    function createCore(
+        uint32 homeDomain,
+        uint32 remoteDomain,
+        address updater,
+        bytes32 committedRoot,
+        uint256 optimisticSeconds,
+        uint256 recoveryTimeLock
+    ) {
+        xAppConnectionManagerImplementation = new XAppConnectionManager();
+        xAppConnectionManagerController = new UpgradeBeaconController();
+        xAppConnectionManagerBeacon = new UpgradeBeacon(
+            address(xAppConnectionManagerImplementation),
+            address(xAppConnectionManagerController)
+        );
+        bytes memory data = abi.encode(hex"");
 
-    function createCore() {}
+        xAppConnectionManager = new UpgradeBeaconProxy(
+            address(xAppConnectionManagerBeacon),
+            data
+        );
 
-    function createGov() {}
+        governanceRouterImplementation = new governaceRouter(
+            homeDomain,
+            recoveryTimeLock
+        );
+        governanceRouterController = new UpgradeBeaconController();
+        governanceRouterBeacon = new UpgradeBeacon(
+            address(governanceRouterImplementation),
+            address(governanceRouterController)
+        );
+        bytes memory data = abi.encode(
+            address(xAppConnectionManager),
+            recoveryManager
+        );
+        governanceRouter = new UpgradeBeaconProxy(
+            address(governanceRouterBeacon),
+            data
+        );
 
-    function setUpdater() {}
+        homeImplementation = new Home(uint32(0));
+        homeController = new UpgradeBeaconController();
+        homeBeacon = new UpgradeBeacon(
+            address(homeImplementation),
+            address(homeController)
+        );
+        bytes memory data = abi.encode(homeDomain);
+        home = new UpgradeBeaconProxy(address(homeBeacon), data);
+
+        replicaImplementation = new Replica(uint32(0));
+        replicaController = new UpgradeBeaconController();
+        replicaBeacon = new UpgradeBeacon(
+            address(replicaImplementation),
+            address(replicaController)
+        );
+        bytes memory data = abi.encode(
+            remoteDomain,
+            updater,
+            committedRoot,
+            optimisticSeconds
+        );
+        replica = new UpgradeBeaconProxy(address(replicaBeacon), data);
+
+        updaterManagerImplementation = new updaterManager(uint32(0));
+        updaterManagerController = new UpgradeBeaconController();
+        updaterManagerBeacon = new UpgradeBeacon(
+            address(updaterManagerImplementation),
+            address(updaterManagerController)
+        );
+        bytes memory data = abi.encode(hex"");
+        updaterManager = new UpgradeBeaconProxy(
+            address(updaterManagerBeacon),
+            data
+        );
+    }
+
+    function enrollWatcherToGovRouter() {
+        xAppConnectionManager.ownerEnrollReplica(
+            address(replica),
+            remoteDomain
+        );
+    }
+
+    function setHome() {
+        updaterManager.setHome(address(home));
+        xAppConnectionManager.setHome(homeDomain);
+    }
+
+    function relinquishControl() {
+        governanceRouter.transferOwnership(address(governanceRouter));
+        xAppConnectionManager.transferOwnership(address(governanceRouter));
+        replicaController.transferOwnership(address(governanceRouter));
+        homeController.transferOwnership(address(governanceRouter));
+        updaterManager.transferOwnership(address(governanceRouter));
+    }
 }
