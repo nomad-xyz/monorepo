9a47b169c7b481902e2d81a6a25a95792368ef05
Imti Majeed <imtiazmaj@gmail.com>
2022-08-18 16:56:29 -0400

    fix: multi-provider and sdk-govern tests now running with jest

--- packages/multi-provider/tests/utils.test.ts
+++ packages/multi-provider/tests/utils.test.ts
@@ -1,12 +1,7 @@
-// import { describe, it } from 'mocha';
 import * as utils from '../src/utils';
 import { hexlify } from '@ethersproject/bytes';
 
-import { expect } from 'chai';
-// import { MultiProvider, Contracts } from '@nomad-xyz/multi-provider';
-// import { ethers } from 'ethers';
-
-describe('multi-provider utils', async () => {
+describe('multi-provider utils', () => {
   const tooLong = new Uint8Array(Array(33).fill(1));
   const badLen = new Uint8Array(Array(15).fill(1));
   const address = new Uint8Array(Array(20).fill(1));
@@ -18,44 +13,44 @@ describe('multi-provider utils', async () => {
 
   it('domain for given chain ID', () => {
     const domain = utils.chainIdToDomain(1);
-    expect(domain).to.equal(6648936);
-    expect(() => utils.chainIdToDomain(0)).to.throw();
+    expect(domain).toEqual(6648936);
+    expect(() => utils.chainIdToDomain(0)).toThrow();
   });
 
   it('gets hex domain from string', () => {
     const ethHex = utils.getHexDomainFromString('eth');
-    expect(ethHex).to.equal('0x657468');
+    expect(ethHex).toEqual('0x657468');
     const moonbeamHex = utils.getHexDomainFromString('beam');
-    expect(moonbeamHex).to.equal('0x6265616d');
+    expect(moonbeamHex).toEqual('0x6265616d');
   });
 
   it('converts to a 32-byte cannonized ID', () => {
     const cannonizedAddress = utils.canonizeId(address);
-    expect(cannonizedAddress.toString()).to.equal(paddedAddress.toString());
+    expect(cannonizedAddress.toString()).toEqual(paddedAddress.toString());
     const cannonizedBytes32 = utils.canonizeId(bytes32);
-    expect(cannonizedBytes32.toString()).to.equal(bytes32.toString());
-    expect(() => utils.canonizeId(tooLong)).to.throw('Too long');
-    expect(() => utils.canonizeId(badLen)).to.throw(
+    expect(cannonizedBytes32.toString()).toEqual(bytes32.toString());
+    expect(() => utils.canonizeId(tooLong)).toThrow('Too long');
+    expect(() => utils.canonizeId(badLen)).toThrow(
       'bad input, expect address or bytes32',
     );
   });
 
   it('converts Nomad Id to emv address', () => {
-    expect(() => utils.evmId(badLen)).to.throw(
+    expect(() => utils.evmId(badLen)).toThrow(
       'Invalid id length. expected 20 or 32. Got 15',
     );
     let evmId = utils.evmId(bytes32);
-    expect(evmId).to.equal(hexlify(address));
-    expect(evmId.length).to.equal(42);
+    expect(evmId).toEqual(hexlify(address));
+    expect(evmId.length).toEqual(42);
     evmId = utils.evmId(paddedAddress);
-    expect(evmId).to.equal(hexlify(address));
-    expect(evmId.length).to.equal(42);
+    expect(evmId).toEqual(hexlify(address));
+    expect(evmId.length).toEqual(42);
     evmId = utils.evmId(address);
-    expect(evmId).to.equal(hexlify(address));
-    expect(evmId.length).to.equal(42);
+    expect(evmId).toEqual(hexlify(address));
+    expect(evmId.length).toEqual(42);
   });
 
-  it.skip('delays x milliseconds', async () => {
+  it.skip('delays x milliseconds', () => {
     // TODO:
   });
 });
