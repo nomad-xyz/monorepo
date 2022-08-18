0aad90f730f6ec47a5b7b8b749b2581e6555de52
Imti Majeed <imtiazmaj@gmail.com>
2022-08-18 16:31:49 -0400

    fix: deploy tests now running with jest

--- packages/deploy/tests/index.test.ts
+++ packages/deploy/tests/index.test.ts
@@ -1,11 +1,7 @@
-// import { describe, it } from 'mocha';
-import { expect } from 'chai';
-
 import * as config from '@nomad-xyz/configuration';
-
 import * as deploy from '../src';
 
-describe('deploy', async () => {
+describe('deploy', () => {
   describe('DeployContext', () => {
     it('converts itself to a NomadContext and registers providers', async () => {
       const conf = config.getBuiltin('development');
@@ -14,7 +10,7 @@ describe('deploy', async () => {
       ctx.registerRpcProvider('rinkeby', 'http://dummyurl.com');
 
       const nomadCtx = ctx.asNomadContext;
-      expect(nomadCtx.getConnection('rinkeby')).to.not.be.undefined;
+      expect(nomadCtx.getConnection('rinkeby')).toBeDefined;
     });
   });
 });
