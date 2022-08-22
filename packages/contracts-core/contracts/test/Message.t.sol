836c4259d07307bd7ee9f3054037078fdd80bff3
Odysseas.eth <odyslam@gmail.com>
2022-08-22 17:02:56 +0300

    feat: test all message functions

--- packages/contracts-core/contracts/test/Message.t.sol
+++ packages/contracts-core/contracts/test/Message.t.sol
@@ -12,19 +12,42 @@ contract MessageTest is Test {
     using TypedMemView for bytes29;
     using Message for bytes29;
 
-    function setUp() public {}
+    // Message components
+    uint32 originDomain;
+    bytes32 sender;
+    uint32 nonce;
+    uint32 destinationDomain;
+    bytes32 recipient;
+    bytes body;
+
+    bytes message;
+    bytes32 messageHash;
+    bytes29 messageView;
+
+    function setUp() public {
+        originDomain = 1000;
+        sender = TypeCasts.addressToBytes32(vm.addr(1));
+        nonce = 24;
+        destinationDomain = 12340;
+        recipient = TypeCasts.addressToBytes32(vm.addr(2));
+        body = hex"E93F";
+        message = abi.encodePacked(
+            originDomain,
+            sender,
+            nonce,
+            destinationDomain,
+            recipient,
+            body
+        );
+        messageHash = keccak256(message);
+        messageView = message.ref(0);
+    }
 
     function test_prefixIs76() public {
         assertEq(Message.PREFIX_LENGTH, 76);
     }
 
     function test_formatMessage() public {
-        uint32 originDomain = 1000;
-        bytes32 sender = TypeCasts.addressToBytes32(vm.addr(1));
-        uint32 nonce = 24;
-        uint32 destinationDomain = 12340;
-        bytes32 recipient = TypeCasts.addressToBytes32(vm.addr(2));
-        bytes memory body = hex"E93F";
         assertEq(
             Message.formatMessage(
                 originDomain,
@@ -34,14 +57,50 @@ contract MessageTest is Test {
                 recipient,
                 body
             ),
-            abi.encodePacked(
+            message
+        );
+    }
+
+    function test_messageHash() public {
+        assertEq(
+            Message.messageHash(
                 originDomain,
                 sender,
                 nonce,
                 destinationDomain,
                 recipient,
                 body
-            )
+            ),
+            messageHash
         );
+    }
+
+    function test_origin() public {
+        console2.logBytes(messageView.clone());
+        assertEq(uint256(Message.origin(messageView)), uint256(originDomain));
+    }
+
+    function test_sender() public {
+        assertEq(Message.sender(messageView), sender);
+    }
+
+    function test_nonce() public {
+        assertEq(uint256(Message.nonce(messageView)), uint256(nonce));
+    }
+
+    function test_destination() public {
+        assertEq(uint256(Message.nonce(messageView)), uint256(nonce));
+    }
+
+    function test_recipient() public {
+        assertEq(Message.recipient(messageView), recipient);
+    }
+
+    function test_body() public {
+        assertEq(Message.body(messageView).keccak(), body.ref(0).keccak());
+    }
+
+    function test_leaf() public {
+        assertEq(Message.leaf(messageView), messageHash);
     }
 }
