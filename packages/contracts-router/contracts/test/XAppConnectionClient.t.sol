4f9b9b3567231f488b65e116a6248b7a25e9f8cf
Odysseas.eth <odyslam@gmail.com>
2022-08-24 15:44:24 +0300

    test: rest of connectionClient tests

--- packages/contracts-router/contracts/test/XAppConnectionClient.t.sol
+++ packages/contracts-router/contracts/test/XAppConnectionClient.t.sol
@@ -34,4 +34,32 @@ contract XAppConnectionClientTest is Test {
             address(xAppCnMngr2)
         );
     }
+
+    function test_setXAppConnectionManagerOnlyOwner() public {
+        address notOwner = vm.addr(12334);
+        vm.startPrank(notOwner);
+        vm.expectRevert("Ownable: caller is not the owner");
+        xAppCnCl.setXAppConnectionManager(address(vm.addr(123123)));
+    }
+
+    function test_exposedHome() public {
+        assertEq(address(xAppCnCl.exposed_home()), home);
+        assertEq(address(xAppCnCl.exposed_home()), address(xAppCnMngr.home()));
+    }
+
+    function test_isReplica() public {
+        address notReplica = vm.addr(1231335234);
+        address replica = vm.addr(123213);
+        uint32 domain = 2134;
+        assert(!xAppCnCl.exposed_isReplica(notReplica));
+        xAppCnMngr.ownerEnrollReplica(replica, domain);
+        assert(xAppCnCl.exposed_isReplica(replica));
+    }
+
+    function test_localDomain() public {
+        uint32 localDomain = 1234;
+        Home homeContract = new Home(localDomain);
+        xAppCnMngr.setHome(address(homeContract));
+        assertEq(uint256(xAppCnCl.exposed_localDomain()), uint256(localDomain));
+    }
 }
