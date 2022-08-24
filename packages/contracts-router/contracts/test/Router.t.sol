ae7e7683c03f510e695972abfd68c4e1eb643db7
Odysseas.eth <odyslam@gmail.com>
2022-08-24 17:40:23 +0300

    chore: test skeleton

---
+++ packages/contracts-router/contracts/test/Router.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity 0.7.6;
+
+import "forge-std/Test.sol";
+import {RouterHarness} from "./harnesses/RouterHarness.sol";
+
+contract RouterTest is Test {}
---
+++ packages/contracts-router/contracts/test/harnesses/RouterHarness.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity 0.7.6;
+
+import {Router} from "../../Router.sol";
+
+contract RouterHarness is Router {
+    function exposed_mustHaveRemote(uint32 domain)
+        external
+        view
+        returns (bytes32)
+    {
+        return _mustHaveRemote(domain);
+    }
+
+    function exposed_isRemoteRouter(uint32 domain, bytes32 router)
+        external
+        view
+        returns(bool)
+    {
+        returns _isRemoteRouter();
+    }
+}
