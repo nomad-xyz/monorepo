08900a5cca58edc77263216732804671705ef563
Odysseas.eth <odyslam@gmail.com>
2022-09-01 18:27:23 +0300

    test: ensureLocalToken

--- packages/contracts-bridge/contracts/test/TokenRegistry.t.sol
+++ packages/contracts-bridge/contracts/test/TokenRegistry.t.sol
@@ -55,4 +55,75 @@ contract TokenRegistryTest is BridgeTest {
         );
         assertEq(repr, localAddress);
     }
+
+    event TokenDeployed(
+        uint32 indexed domain,
+        bytes32 indexed id,
+        address indexed representation
+    );
+
+    function test_ensureLocalTokenDeploy() public {
+        uint32 newDomain = 13;
+        bytes32 newId = bytes32("hey yoou");
+        // It's the second contract that is been deployed by tokenRegistry
+        // It deploys a bridgeToken during setUp() of BridgeTest
+        address calculated = computeCreateAddress(address(tokenRegistry), 2);
+        vm.expectEmit(true, true, true, false);
+        emit TokenDeployed(newDomain, newId, calculated);
+        vm.prank(tokenRegistry.owner());
+        address deployed = tokenRegistry.ensureLocalToken(newDomain, newId);
+        assertEq(deployed, calculated);
+    }
+
+    function test_ensureLocalTokenExisting() public {
+        // It's the second contract that is been deployed by tokenRegistry
+        // It deploys a bridgeToken during setUp() of BridgeTest
+        vm.prank(tokenRegistry.owner());
+        address addr = tokenRegistry.ensureLocalToken(
+            remoteDomain,
+            remoteTokenRemoteAddress
+        );
+        assertEq(addr, remoteTokenLocalAddress);
+    }
+
+    function test_ensureLocalTokenOnlyOwner() public {
+        // It's the second contract that is been deployed by tokenRegistry
+        // It deploys a bridgeToken during setUp() of BridgeTest
+        vm.expectRevert("Ownable: caller is not the owner");
+        address addr = tokenRegistry.ensureLocalToken(
+            remoteDomain,
+            remoteTokenRemoteAddress
+        );
+    }
+
+    uint256 iterations;
+
+    function test_ensureLocalTokenDeployFuzzed(uint32 domain, bytes32 id)
+        public
+    {
+        // It's the second contract that is been deployed by tokenRegistry
+        // It deploys a bridgeToken during setUp() of BridgeTest
+        address calculated = computeCreateAddress(
+            address(tokenRegistry),
+            2 + iterations
+        );
+        vm.expectEmit(true, true, true, false);
+        emit TokenDeployed(domain, id, calculated);
+        vm.prank(tokenRegistry.owner());
+        address deployed = tokenRegistry.ensureLocalToken(domain, id);
+        assertEq(deployed, calculated);
+        iterations++;
+    }
+
+    function test_ensureLocalTokenOnlyOwnerFuzzed(address user) public {
+        // It's the second contract that is been deployed by tokenRegistry
+        // It deploys a bridgeToken during setUp() of BridgeTest
+        vm.assume(user != tokenRegistry.owner());
+        vm.expectRevert("Ownable: caller is not the owner");
+        vm.prank(user);
+        address addr = tokenRegistry.ensureLocalToken(
+            remoteDomain,
+            remoteTokenRemoteAddress
+        );
+    }
 }
