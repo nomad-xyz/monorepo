70d757b3c9c1204723b85501886196b7faeadb72
Imti Majeed <imtiazmaj@gmail.com>
2022-09-02 17:34:20 -0500

    chore: update nomad-cli configs to match rest of packages (#445)
    * chore: update nomad-cli configs to match rest of packages
    
    * chore: bump yarn.lock

---
+++ packages/nomad-cli/tests/helpers/init.js
@@ -0,0 +1,9 @@
+// const path = require('path');
+// process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json');
+// process.env.NODE_ENV = 'development';
+
+// global.oclif = global.oclif || {};
+// global.oclif.columns = 80;
+
+// TODO: determine what this is used for and
+// if we still need it when moving to jest
