29f83deda9d029b2a0cf29b191a8ce329eb59788
Luke Tchang <ltchang@stanford.edu>
2022-09-07 16:29:59 -0700

    allow passing of values through env vars

--- packages/contracts-da-bridge/contracts/script/DeployDABridge.s.sol
+++ packages/contracts-da-bridge/contracts/script/DeployDABridge.s.sol
@@ -3,23 +3,38 @@ pragma solidity 0.7.6;
 
 import "forge-std/Script.sol";
 import {XAppConnectionManager} from "@nomad-xyz/contracts-core/contracts/XAppConnectionManager.sol";
+import {Replica} from "@nomad-xyz/contracts-core/contracts/Replica.sol";
 import {DABridgeRouter} from "../DABridgeRouter.sol";
 
 contract DeployDABridgeRouter is Script {
-    address constant HOME_ADDRESS = 0x0000000000000000000000000000000000000000;
-    address constant REPLICA_ADDRESS =
-        0x0000000000000000000000000000000000000000;
-    uint32 constant REPLICA_DOMAIN = uint32(0);
+    uint256 localDomain;
+    uint256 remoteDomain;
+    address remoteUpdater;
+    uint256 optimisticSeconds;
+
+    function loadEnvVars() public {
+        localDomain = vm.envUint("LOCAL_DOMAIN");
+        remoteDomain = vm.envUint("REMOTE_DOMAIN");
+        remoteUpdater = vm.envAddress("REMOTE_UPDATER");
+        optimisticSeconds = vm.envUint("OPTIMISTIC_SECONDS");
+    }
 
     function run() external {
         vm.startBroadcast();
 
+        Replica replica = new Replica(uint32(localDomain));
+        replica.initialize(
+            uint32(remoteDomain),
+            remoteUpdater,
+            bytes32(0),
+            optimisticSeconds
+        );
+
         XAppConnectionManager manager = new XAppConnectionManager();
-        manager.setHome(HOME_ADDRESS);
-        manager.ownerEnrollReplica(REPLICA_ADDRESS, REPLICA_DOMAIN);
+        manager.ownerEnrollReplica(address(replica), uint32(remoteDomain));
 
         DABridgeRouter router = new DABridgeRouter();
-        router.setXAppConnectionManager(address(manager));
+        router.initialize(address(manager));
 
         vm.stopBroadcast();
     }
