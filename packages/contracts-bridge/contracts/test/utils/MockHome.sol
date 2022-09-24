c8f2b7dc84b23af17244b964c8b0a68b52275fa2
Odysseas.eth <odyslam@gmail.com>
2022-09-24 19:51:14 -0400

    feat: make mockHome support multiple hack dispatch events

--- packages/contracts-bridge/contracts/test/utils/MockHome.sol
+++ packages/contracts-bridge/contracts/test/utils/MockHome.sol
@@ -7,6 +7,8 @@ contract MockHome {
     uint32 public localDomain;
     uint256 counter;
     mapping(uint32 => uint32) public nonces;
+    uint256 hackCounter;
+    mapping(uint32 => uint32) public hackNonces;
     bytes32 public committedRoot;
     uint256 public constant MAX_MESSAGE_BODY_BYTES = 2 * 2**10;
 
@@ -54,7 +56,8 @@ contract MockHome {
         bytes memory _messageBody,
         address sender
     ) external {
-        uint32 _nonce = nonces[_destinationDomain];
+        uint32 _nonce = hackNonces[_destinationDomain];
+        hackNonces[_destinationDomain] = _nonce + 1;
         bytes memory _message = Message.formatMessage(
             localDomain,
             bytes32(uint256(uint160(sender))),
@@ -66,7 +69,7 @@ contract MockHome {
         bytes32 _messageHash = keccak256(_message);
         emit Dispatch(
             _messageHash,
-            counter,
+            hackCount() - 1,
             _destinationAndNonce(_destinationDomain, _nonce),
             committedRoot,
             _message
@@ -76,6 +79,11 @@ contract MockHome {
     function count() public returns (uint256) {
         counter = counter + 1;
         return counter;
+    }
+
+    function hackCount() public returns (uint256) {
+        hackCounter = hackCounter + 1;
+        return hackCounter;
     }
 
     function _destinationAndNonce(uint32 _destination, uint32 _nonce)
