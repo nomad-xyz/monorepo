3446de36ca2a92269a2c0aeabacb3992b52037d5
Odysseas.eth <odyslam@gmail.com>
2022-10-08 09:58:20 -0500

    feat: add tests for setDetails, permit, domainSeparator

--- packages/contracts-bridge/contracts/test/BridgeToken.t.sol
+++ packages/contracts-bridge/contracts/test/BridgeToken.t.sol
@@ -58,7 +58,7 @@ contract BridgeTokenTest is Test {
         uint8 indexed decimals
     );
 
-    function test_setDetailsHash(
+    function test_setDetailsHashAndSetDetails(
         string memory name,
         string memory symbol,
         uint8 decimals
@@ -76,6 +76,17 @@ contract BridgeTokenTest is Test {
         vm.expectEmit(true, true, true, false);
         emit UpdateDetails(name, symbol, decimals);
         token.setDetails(name, symbol, decimals);
+        assertEq(token.name(), name);
+        assertEq(token.symbol(), symbol);
+        assertEq(token.decimals(), uint256(decimals));
+    }
+
+    function test_setDailtsFailSecondTime() public {
+        string memory name = "Numenor";
+        string memory symbol = "NM";
+        uint8 decimals = 19;
+        vm.expectRevert("!committed details");
+        token.setDetails(name, symbol, decimals);
     }
 
     function test_setDetailsHashOwner() public {
@@ -83,6 +94,167 @@ contract BridgeTokenTest is Test {
         vm.prank(address(0xBEEF));
         vm.expectRevert("Ownable: caller is not the owner");
         token.setDetailsHash(hash);
+    }
+
+    function test_domainSeperator() public {
+        uint256 _chainId;
+        assembly {
+            _chainId := chainid()
+        }
+        bytes32 sep = keccak256(
+            abi.encode(
+                keccak256(
+                    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                ),
+                keccak256(bytes(token.name())),
+                keccak256(bytes("1")),
+                _chainId,
+                address(token)
+            )
+        );
+        assertEq(sep, token.domainSeparator());
+    }
+
+    function test_permitSuccess() public {
+        uint256 key = 123;
+        address owner = vm.addr(key);
+        address toPermit = address(0xBEEF);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            key,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.domainSeparator(),
+                    keccak256(
+                        abi.encode(
+                            PERMIT_TYPEHASH,
+                            owner,
+                            toPermit,
+                            1e18,
+                            0,
+                            block.timestamp
+                        )
+                    )
+                )
+            )
+        );
+        token.permit(owner, toPermit, 1e18, block.timestamp, v, r, s);
+        assertEq(token.allowance(owner, toPermit), uint256(1e18));
+        assertEq(token.nonces(owner), uint256(1));
+    }
+
+    function test_permitRevertsBadNonce() public {
+        uint256 key = 123;
+        address owner = vm.addr(key);
+        address toPermit = address(0xBEEF);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            key,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.domainSeparator(),
+                    keccak256(
+                        abi.encode(
+                            PERMIT_TYPEHASH,
+                            owner,
+                            toPermit,
+                            1e18,
+                            1,
+                            block.timestamp
+                        )
+                    )
+                )
+            )
+        );
+        vm.expectRevert("ERC20Permit: invalid signature");
+        token.permit(owner, toPermit, 1e18, block.timestamp, v, r, s);
+    }
+
+    function test_permitRevertsBadDeadline() public {
+        uint256 key = 123;
+        address owner = vm.addr(key);
+        address toPermit = address(0xBEEF);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            key,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.domainSeparator(),
+                    keccak256(
+                        abi.encode(
+                            PERMIT_TYPEHASH,
+                            owner,
+                            toPermit,
+                            1e18,
+                            0,
+                            block.timestamp
+                        )
+                    )
+                )
+            )
+        );
+        vm.expectRevert("ERC20Permit: invalid signature");
+        token.permit(owner, toPermit, 1e18, block.timestamp + 1, v, r, s);
+    }
+
+    function test_permitRevertsPastDeadline() public {
+        uint256 key = 123;
+        address owner = vm.addr(key);
+        address toPermit = address(0xBEEF);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            key,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.domainSeparator(),
+                    keccak256(
+                        abi.encode(
+                            PERMIT_TYPEHASH,
+                            owner,
+                            toPermit,
+                            1e18,
+                            0,
+                            block.timestamp - 1
+                        )
+                    )
+                )
+            )
+        );
+        vm.expectRevert("ERC20Permit: expired deadline");
+        token.permit(owner, toPermit, 1e18, block.timestamp - 1, v, r, s);
+    }
+
+    function test_permitRevertsOnReplay() public {
+        uint256 key = 123;
+        address owner = vm.addr(key);
+        address toPermit = address(0xBEEF);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            key,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.domainSeparator(),
+                    keccak256(
+                        abi.encode(
+                            PERMIT_TYPEHASH,
+                            owner,
+                            toPermit,
+                            1e18,
+                            0,
+                            block.timestamp
+                        )
+                    )
+                )
+            )
+        );
+        token.permit(owner, toPermit, 1e18, block.timestamp, v, r, s);
+        vm.expectRevert("ERC20Permit: invalid signature");
+        token.permit(owner, toPermit, 1e18, block.timestamp, v, r, s);
     }
 
     function test_transferOwnership() public {
