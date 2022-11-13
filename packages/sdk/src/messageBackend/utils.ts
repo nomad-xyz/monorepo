6e19c6b70b1718da6955f01097f9294850fb7089
Daniil Naumetc <daniil.naumetc@gmail.com>
2022-11-13 18:50:59 +0300

    nits: comments

--- packages/sdk/src/messageBackend/utils.ts
+++ packages/sdk/src/messageBackend/utils.ts
@@ -1,9 +1,11 @@
+// Disable eslint to allow any type for the following function to work with any types
+/* eslint-disable */ 
+
 /**
  * Replaces all null values in an object with `undefined`
  * @param obj an object
  * @returns object with no null values
  */
-/* eslint-disable */ 
 export function nulls2undefined(obj: any): any {
   if (obj === null) {
     return undefined;
