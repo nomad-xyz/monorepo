af9244506e9cb55fc4d445d48fb89365e0c81116
Imti Majeed <imtiazmaj@gmail.com>
2022-11-14 16:36:33 -0600

    wip: add basic structure, add todos

--- packages/monitor/src/server.ts
+++ packages/monitor/src/server.ts
@@ -187,3 +187,5 @@ export class MonitoringCollector extends MetricsCollector {
     this.goldskyErrors.labels(code, query, this.environment).inc();
   }
 }
+// TODO: might want to just copy the MetricsCollector from here:
+// https://github.com/nomad-xyz/monorepo/blob/d5b7d05190bc2493e17742fc65f032b5e94be78e/packages/indexer/src/core/metrics.ts
