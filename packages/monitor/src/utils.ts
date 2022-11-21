9a9c2b786a5917f44617872d4b2c04e96f7e120a
Imti Majeed <imtiazmaj@gmail.com>
2022-11-21 16:08:32 -0500

    feat: nft related counters to MetricsCollector, add TODOs

--- packages/monitor/src/utils.ts
+++ packages/monitor/src/utils.ts
@@ -1,25 +1,26 @@
 import Logger from 'bunyan';
 
 export enum BunyanLevel {
-    FATAL = 'fatal',
-    ERROR = 'error',
-    WARN = 'warn',
-    INFO = 'info',
-    DEBUG = 'debug',
-    TRACE = 'trace',
-  }
-  
-  export function createLogger(
-    name: string,
-    environment: string,
-    level?: BunyanLevel,
-  ): Logger {
-    return Logger.createLogger({
-      name,
-      serializers: Logger.stdSerializers,
-      level: level || 'debug',
-      environment: environment,
-    });
-  }
+  FATAL = 'fatal',
+  ERROR = 'error',
+  WARN = 'warn',
+  INFO = 'info',
+  DEBUG = 'debug',
+  TRACE = 'trace',
+}
 
-  export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+export function createLogger(
+  name: string,
+  environment: string,
+  level?: BunyanLevel,
+): Logger {
+  return Logger.createLogger({
+    name,
+    serializers: Logger.stdSerializers,
+    level: level || 'debug',
+    environment: environment,
+  });
+}
+
+export const sleep = (ms: number): Promise<void> =>
+  new Promise((res) => setTimeout(res, ms));
