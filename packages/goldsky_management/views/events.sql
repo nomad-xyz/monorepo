7b99437578141c1ac562bd9c17ca6d1cbd69162d
Daniil Naumetc <daniil.naumetc@gmail.com>
2022-11-30 11:57:55 +0000

    nits: moved from block_number to block

--- packages/goldsky_management/views/events.sql
+++ packages/goldsky_management/views/events.sql
@@ -23,21 +23,21 @@ SELECT
   decoded_dispatch.message__action__details_hash,
   decoded_dispatch.message_type,
   decoded_dispatch.transaction_hash AS dispatch_tx,
-  decoded_dispatch.block_number AS dispatch_block,
+  decoded_dispatch.block AS dispatch_block,
   decoded_dispatch."timestamp" AS dispatched_at,
 update.transaction_hash AS update_tx,
-update.block_number AS update_block,
+update.block AS update_block,
 update."timestamp" AS updated_at,
 update.old_root,
 update.new_root,
 update.signature,
 update.gs_chain_id AS update_chain_id,
   relay.transaction_hash AS relay_tx,
-  relay.block_number AS relay_block,
+  relay.block AS relay_block,
   relay."timestamp" AS relayed_at,
   relay.gs_chain_id AS relay_chain_id,
   process.transaction_hash AS process_tx,
-  process.block_number AS process_block,
+  process.block AS process_block,
   process."timestamp" AS processed_at
 FROM
   (
@@ -49,7 +49,7 @@ FROM
             update_1.old_root,
             update_1.new_root,
             update_1.signature,
-            update_1.block_number,
+            update_1.block,
             update_1.transaction_hash,
             update_1."timestamp",
             update_1.gs_chain_id
@@ -72,7 +72,7 @@ FROM
       LEFT JOIN (
         SELECT
           relay_1.transaction_hash,
-          relay_1.block_number,
+          relay_1.block,
           relay_1."timestamp",
           relay_1.old_root,
           relay_1.gs_chain_id
